5ca57ca0df314c56dd11e18d74053982841bc9f7
Mr.Worldwide <syahfis@matdespatch.com>
2018-11-11 14:26:00 +0800

    simple layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProfileStatusComponent } from './profile-status/profile-status.component';
+import { QuizContainerComponent } from './quiz-container/quiz-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileStatusComponent,
+    QuizContainerComponent
   ],
   imports: [
     BrowserModule
